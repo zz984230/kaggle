d995f1cb3d586d4ecb9fa549d58c139d82f5ee56
zero <zz984230@users.noreply.github.com>
2019-04-15 17:53:22 +0800

    Update classified_model.py

--- credit_score/code/classified_model.py
+++ credit_score/code/classified_model.py
@@ -1,16 +1,21 @@
 from sklearn import linear_model, metrics
-from kaggle.credit_score.code.credit_data import CreditPreprocessData
+from dl_group.credit_score.code.credit_data import CreditPreprocessData
+from dl_group.credit_score.code.ann_model import ANN
 from scipy import interp
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import model_selection
-from sklearn import svm
+from sklearn import model_selection, svm, ensemble
 from xgboost import XGBClassifier
 import pandas as pd
 
 
 class ClassifiedModel(object):
-    def __init__(self, ori_X, ori_y, test_X=None, cls_name="logicalRegression", saved_predict_path="../data/result/result.csv"):
+    def __init__(self,
+                 ori_X,
+                 ori_y,
+                 test_X=None,
+                 cls_name="logicalRegression",
+                 saved_predict_path="../data/result/result.csv"):
         self.__ori_X = ori_X
         self.__ori_y = ori_y
         self.__train_X = None
@@ -30,17 +35,13 @@ class ClassifiedModel(object):
         self.__train_X, self.__val_X, self.__train_y, self.__val_y = model_selection.train_test_split(self.__ori_X, self.__ori_y, test_size=0.3, random_state=0)
 
     def __logical_regression_model(self):
-        self.__cls = linear_model.LogisticRegression().fit(self.__train_X, self.__train_y)
-        score = self.__cls.score(self.__val_X, self.__val_y)
-        print(score)
+        self.__cls = linear_model.LogisticRegression().fit(self.__ori_X, self.__ori_y)
 
     def __svc_model(self):
-        self.__cls = svm.SVC().fit(self.__train_X, self.__train_y)
-        score = self.__cls.score(self.__val_X, self.__val_y)
-        print(score)
+        self.__cls = svm.SVC().fit(self.__ori_X, self.__ori_y)
 
     def __xgboost_model(self):
-        self.__cls = XGBClassifier()
+        self.__cls = XGBClassifier(learning_rate=0.2, reg_alpha=1, subsample=0.9)
 
         # score = model_selection.cross_val_score(self.__cls, self.__train_X, self.__train_y, cv=5, scoring="roc_auc")
         # print(score)
@@ -50,6 +51,15 @@ class ClassifiedModel(object):
 
         self.__cls.fit(self.__ori_X, self.__ori_y)
 
+    def __random_forest_model(self):
+        self.__cls = ensemble.RandomForestClassifier(n_estimators=100)
+        self.__cls.fit(self.__ori_X, self.__ori_y)
+
+    def __ann_model(self):
+        self.__cls = ANN(epoch=30)
+        # self.__cls.fit(self.__ori_X, self.__ori_y)
+        self.__cls.fit_by_saved_model(self.__ori_X, self.__ori_y)
+
     def train(self):
         self.__simple_split_train_data()
         if self.__cls_name == "logicalRegression":
@@ -58,10 +68,13 @@ class ClassifiedModel(object):
             self.__svc_model()
         elif self.__cls_name == "xgboost":
             self.__xgboost_model()
+        elif self.__cls_name == "randomForest":
+            self.__random_forest_model()
+        elif self.__cls_name == "ANN":
+            self.__ann_model()
 
     def predict(self):
         self.__predict_result = self.__cls.predict_proba(self.__test_X)
-        print(np.shape(self.__predict_result))
 
     def save_predict_result(self):
         l = np.reshape([i + 1 for i in range(np.shape(self.__predict_result)[0])], [-1, 1])
@@ -76,11 +89,11 @@ if __name__ == "__main__":
     feature_select_credit = CreditPreprocessData("../data/preprocess/SMOTE_train.csv")
     feature_select_credit.load_oversampling_train_data()
     feature_select_credit.load_cleaned_test_data()
-    # feature_select_credit.rfe_selected_train_data()
+    # feature_select_credit.xgboost_selected_data(is_plot=True)
     train_X, train_y = feature_select_credit.selected_train_feature_label_data
     test_X = feature_select_credit.selected_test_feature_data
 
-    cls_model = ClassifiedModel(train_X, train_y, test_X=test_X, cls_name="xgboost")
+    cls_model = ClassifiedModel(train_X, train_y, test_X=test_X, cls_name="ANN")
     cls_model.train()
     cls_model.predict()
     cls_model.save_predict_result()
